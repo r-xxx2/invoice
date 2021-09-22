53d7be661fcde1b02354830186548d1bcc34b4be
ryusuke ota <r.xxx2k@gmail.com>
2021-09-23 02:22:04 +0900

    pyで書き出し

--- invoice.py
+++ invoice.py
@@ -1,3 +1,6 @@
+# To add a new cell, type '# %%'
+# To add a new markdown cell, type '# %% [markdown]'
+# %%
 import pandas as pd
 import openpyxl as px
 import datetime
@@ -24,9 +27,9 @@ print(dte)
 
 
 
-#----------------------
+#----------------------------------
 # 【６】 日付、お振込み期限
-#----------------------
+#----------------------------------
 
 
 #--------- 日本語でstrftimeが表示できないエラーの回避 -------------------------
@@ -52,10 +55,12 @@ next_month = EOM + relativedelta(months=1)
 
 
 
-#----------------------------------------
+
+#----------------------
 # 【２】 請求書一覧から顧客CDをDFに取得
-#----------------------------------------
+#----------------------
 customer_df = pd.read_excel("請求一覧創造太郎.xlsx", sheet_name="顧客管理テーブル", index=0, header=1)
+display(customer_df)
 
 
 
@@ -71,6 +76,8 @@ sheetname = "請求一覧" + invoice_month
 month_df = pd.read_excel("請求一覧創造太郎.xlsx", sheet_name=sheetname, header=2, index=False)
 display(month_df)
 
+month_df = month_df.merge(customer_df, left_on="相手先コード", right_on="顧客ＣＤ", how="left")
+display(month_df)
 
 
 # ４ 相手先コードのユニークを作成
@@ -79,98 +86,11 @@ print(sup_cd)
 sup_cd.sort()
 print(sup_cd)
 
-#------- 月一覧のDFからカラムごとにリスト化 --------------
 
-odr_no = month_df["受注No."].to_list
-customer = month_df["相手先コード"].to_list
-product_name1 = month_df["商品名１"].to_list
-product_name2 = month_df["商品名２"].to_list
-en = month_df["金額（税抜き）"].to_list
-bikou = month_df["備考"].to_list
-
-#-------------------------------------------------------
-print(odr_no)
-
-print(df["受注No."][0])
-
-
-# %%
 #-------------------------------------
 # 【７】 各会社ごとにExcelに出力
 #-------------------------------------
 
-# month_df[month_df["相手先コード"] == 1001]
-
-import openpyxl as px
-
-
-wb = px.load_workbook("請求書創造太郎.xlsx")
-ws = wb["請求書元"]
-
-
-j = 0
-
-for i in range(1,10,2):
-    if j == 4:
-        break
-    # for j in range(0, len(sup_cd)):
-    ws.cell(row= i+17, column=1).value = df["受注No."][j]
-    j += 1
-
-wb.save("2021年test月分_請求書【会社名様】.xlsx")
-
-
-#相手先コードが一致する行を順番に出力
-    # for customer_cd in sup_cd:
-    #     df = month_df[month_df["相手先コード"] == customer_cd] #相手先コードが一致するものを
-    #     print("-----------------")
-    #     print(df["受注No."])
-    #     print(df["商品名１"])
-
-
-
-
-    #         for cell in rows:
-    #     for i in range(16,24):
-    #             ws.cell(row = i+2, column=1, value=str(df["受注No."])) 
-
-
-    # wb.save("2021年11月分_請求書【会社名様】.xlsx")
-
-
-# %%
-import openpyxl as px
-
-ivl_path = "C:/Users/rxxx2/OneDrive/05.ハロトレ/赤柗/01.実制作/code/請求一覧創造太郎.xlsx"
-tpl_path = "C:/Users/rxxx2/OneDrive/05.ハロトレ/赤柗/01.実制作/code/請求書創造太郎.xlsx"
-
-
-wb = px.load_workbook(tpl_path)
-ws = wb["請求書元"]
-
-
-li = [210601, 210701, 210702]
-j = 0
-
-print(len(li))
-
-
-for i in range(1,10,2):
-    for j in range(0, len(li)):
-        ws.cell(row = i+17, column = 1, value=li[j])
-        j += 1
-
-
-wb.save("2021年10月分_請求書【会社名様】.xlsx")
-
-
-# %%
-#日付の出力
-
-import openpyxl as px
-
-wb = px.load_workbook("請求書創造太郎.xlsx")
-ws = wb["請求書元"]
 
 #----------- 出力セルの定義 ----------------
 
@@ -183,19 +103,71 @@ dl_loc = "B35"                 # 振込期限
 #------------------------------------------
 
 
-# 出力コード
-
-ws[date_loc] = EOM.strftime("%Y年%m月%d日") # 日付
-# ws["H2"] =                              # 請求書No.
-# ws["A6"] = xxxxx + "御中"               # 会社名
-# ws["G28"] =                             # 合計金額
-ws[dl_loc] = "お振込み期限　　：　　" + next_month.strftime("%Y年%m月%d日")
 
 
-wb.save("2021年test月分_請求書【会社名様】.xlsx")
+
+###################
+#メインのコード
+###################
 
 
-# %%
+x = 1
+for customer_cd in sup_cd:
+    string = str(x).zfill(2) # 請求書No.用の連番
+    
+    wb = px.load_workbook("請求書創造太郎.xlsx")
+    ws = wb["請求書元"]
 
+    
+    df = month_df[month_df["相手先コード"] == customer_cd]
+    print(display(df))
+    print("---------------")
+    df_row = int(len(df))
+    df_row += 1
+    print(df_row)
+    print(10 + 2)
+    #------- 月一覧のDFからカラムごとにリスト化 --------------
+
+    odr_no = df["受注No."].to_list()
+    cus_cd = df["相手先コード"].to_list()
+    cus_name = df["顧客名"].to_list()
+    product_name1 = df["商品名１"].to_list()
+    product_name2 = df["商品名２"].to_list()
+    en = df["金額（税抜き）"].to_list()
+    bikou = df["備考"].to_list()
+
+    #-------------------------------------------------------
+
+
+    ws[date_loc] = EOM.strftime("%Y年%m月%d日")            # 日付
+    ws[ivno_loc] = invoice_month + string                  # 請求書No.
+    ws[Co_loc] = cus_name[0] + " 御中"                     # 会社名
+    ws[dl_loc] = "お振込み期限　　：　　" + next_month.strftime("%Y年%m月%d日")   # 振込期限
+
+    j = 0
+    for i in range(18,27,2):
+        if j == len(df):
+            break
+        ws.cell(row= i, column=1).value = odr_no[j]           # 受注No.
+        ws.cell(row= i, column=2).value = product_name1[j]    # 商品名１
+        ws.cell(row= i+1, column=2).value = product_name2[j]  # 商品名２
+        ws.cell(row= i, column=5).value = 1                   # 数量
+        ws.cell(row= i, column=6).value = "式"                # 式
+        ws.cell(row= i, column=7).value = en[j]               # 金額
+        ws.cell(row= i, column=8).value = bikou[j]            # 備考
+        j += 1
+
+    total = df["金額（税抜き）"].sum()
+    tax = total * 0.1
+
+    # 商品名蘭の書式設定
+    ws.cell(row= 18+len(df)*2+1 , column=2).value = "以上に掛かる消費税"     # 以上に掛かる消費税
+    ws.cell(row= 18+len(df)*2+2 , column=2).value = "～　以　下　余　白　～"  # ～以下余白～
+    ws.cell(row= 18+len(df)*2 , column=7).value = tax                      # 消費税
+    ws[sum_loc] = total + tax                                                   # 合計金額
+
+    # 書き出しのファイル設定
+    wb.save(EOM.strftime("%Y年%m月") + "分請求書_【" + cus_name[0] + "様】.xlsx")
+    x += 1
 
 
